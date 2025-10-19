b1da90d74136fc4bc0a24f107300d5558a848efc
Usuario <andresfb2002@gmail.com>
2025-10-19 13:36:51 -0500

    names change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./router/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Auth Service funcionando ✅");
+  res.send("Auth Service funcionando");
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Auth Service corriendo en puerto ${process.env.PORT} 🚀`)
+  console.log(`Auth Service corriendo en puerto ${process.env.PORT}`)
 );
